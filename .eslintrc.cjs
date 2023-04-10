d84bfaea210631904e42c77f5154c14cc6cac330
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-10 19:57:13 +0400

    chore(eslint): add import resolver

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,49 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  plugins: ['react', '@typescript-eslint', 'import'],
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier"
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
   ],
-  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-unresolved': 'error',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+  },
+  overrides: [
+    {
+      files: ['vite.config.ts'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
