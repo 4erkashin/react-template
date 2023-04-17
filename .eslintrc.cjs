c732029942868f11fb19d38cadba63175f400554
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-17 18:13:17 +0400

    chore(eslint): disable rule
    'jsx-a11y/label-has-associated-control': 'off'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -43,6 +43,7 @@ module.exports = {
     'import/no-unresolved': 'error',
     'import/prefer-default-export': 'off',
     'react/function-component-definition': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
   },
   overrides: [
     {
