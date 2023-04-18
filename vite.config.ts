bb3a66b928e3a808f82fc7a210647df681c738fe
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-18 20:14:39 +0400

    feat(autoprefixer): add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+// https://vitejs.dev/config/
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
 });
