e6669b3e43cbeb9a78175aa498d7b203ef0bc2e6
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-21 14:30:03 +0400

    fix(vite): fix passing env to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 // https://vitejs.dev/config/
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
 import autoprefixer from 'autoprefixer';
 
-export default defineConfig({
-  base: import.meta.env.BASE_URL,
-  plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
-  css: {
-    postcss: {
-      plugins: [autoprefixer],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
+    css: {
+      postcss: {
+        plugins: [autoprefixer],
+      },
     },
-  },
+  };
 });
